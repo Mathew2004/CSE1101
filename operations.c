0444a8551a97bc2f8960600e61df9fdd492119a8
Evan purification <purificationevan04@gmail.com>
2023-10-09 01:15:04 +0600

    Adding Operations.c FIle

---
+++ operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter first integer:");
+    scanf("%d",&a);
+    printf("\nEnter second integer:");
+    scanf("%d",&b);
+    printf("\nSum is %d",a+b); // for summation
+    printf("\nDifference is %d",a-b); // for subtraction
+    printf("\nProduct is %d",a*b); // for multiplication
+    return 0; 
+
+}
